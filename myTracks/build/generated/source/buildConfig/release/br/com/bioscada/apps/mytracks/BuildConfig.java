8a3b9222a99aec1f6057e8d081556e20da6f3388
schmaedech <schmaedech@gmail.com>
2015-03-27 22:43:18 -0300

    all idea project

---
+++ myTracks/build/generated/source/buildConfig/release/br/com/bioscada/apps/mytracks/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.bioscada.apps.mytracks;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "br.com.bioscada.apps.mytracks";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 74;
+  public static final String VERSION_NAME = "";
+}
